65a4a46469d308cafb2a394d9a6f986aa157850f
youcef baghdadi <youssef.xd.12@gmail.com>
2019-06-09 18:03:35 +0100

    Add WPW Project Add MVVM

---
+++ TRMWPFUserInterface/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMWPFUserInterface.ViewModels;
+namespace TRMWPFUserInterface
+{
+    class Bootstrapper :BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+
+        }
+
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
