ffcc1a358f0069e695e23c0d48768948102ccb6e
youcef baghdadi <youssef.xd.12@gmail.com>
2019-06-11 16:13:13 +0100

    Register DB Tables
    the tables for basic register system

---
+++ TRMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory]
+( 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY ,
+	[ProdcutID] INT NOT NULL  FOREIGN KEY  REFERENCES  [Product](Id) ,
+	[Quantity] INT NOT NULL DEFAULT 1,
+	[PurchasePrice] MONEY NOT NULL ,
+	[PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+ )
---
+++ TRMData/dbo/Tables/UserTable.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserTable]
+(
+
+	[UserId] NVARCHAR(128) PRIMARY KEY,
+	[FirstName] NVARCHAR(50) NOT NULL,
+	[LastName] NVARCHAR(50) NOT NULL,
+	[EmailAddress] NVARCHAR(256) NOT NULL,
+	[CreateDate] DateTime2 NOT NULL DEFAULT getutcdate(),
+	 
+)
